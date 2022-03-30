4fdce11aea6d382eca97d2016440897f17d59962
Matheus <matheus.pgm2000@gmail.com>
2022-03-30 11:00:36 -0300

    feat: adding text reading with voice and click overlay

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const main = document.querySelector('main')
+const buttonInsertText = document.querySelector('.btn-toggle')
+const divTextBox = document.querySelector('.text-box')
+const closeDivTextBox = document.querySelector('.close')
+const selectElement = document.querySelector('select')
 
 const humanExpressions = [
     { img: './img/drink.jpg', text: 'Estou com sede'},
@@ -15,15 +19,56 @@ const humanExpressions = [
     { img: './img/grandma.jpg', text: 'Quero ver a vovó'},
 ]
 
+const utterance = new SpeechSynthesisUtterance()
+
+const setTextMessage = text => {
+    utterance.text = text
+}
+
+const speakText = () => {
+    speechSynthesis.speak(utterance)
+}
+
 const createExpressionBox = ({ img, text }) => {
     const div = document.createElement('div')
+
     div.classList.add('expression-box')
     div.innerHTML = `
         <img src= "${img}" alt= "${text}">
-        <p clas="info">${text}</p>
+        <p class="info">${text}</p>
     `
+    div.addEventListener('click', () => {
+        setTextMessage(text)
+        speakText()
 
+        div.classList.add('active')
+        setTimeout(() => {
+            div.classList.remove('active')
+        }, 1000)
+    })
     main.appendChild(div)
 }
 
 humanExpressions.forEach(createExpressionBox)
+
+let voices = []
+
+speechSynthesis.addEventListener('voiceschanged', () => {
+    voices = speechSynthesis.getVoices()
+
+    voices.forEach(({ name, lang }) => {
+        const option = document.createElement('option')
+
+        option.value = name
+        option.textContent = `${lang} | ${name}`
+        selectElement.appendChild(option)
+    })
+})
+
+buttonInsertText.addEventListener('click', () => {
+    divTextBox.classList.add('show')
+})
+
+closeDivTextBox.addEventListener('click', () => {
+    divTextBox.classList.remove('show')
+})
